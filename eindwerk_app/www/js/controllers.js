d32ff9193aa00e246cb81c7f051533d22ba7910a
unknown <maicopaulussen@hotmail.be>
2015-05-05 18:53:51 +0200

    filter bug fixes

--- eindwerk_app/www/js/controllers.js
+++ eindwerk_app/www/js/controllers.js
@@ -208,72 +208,100 @@ angular.module('starter.controllers', [])
 })
 
 .controller('SettingsCtrl', function($scope, $stateParams,$timeout,$location,$http) {
+  var delayName;
   $scope.changeUsername = function(){
+    $timeout.cancel(delayName);
+    delayName = $timeout(function() {
       $scope.postChanges('username',$scope.userdata.username); 
+    }, 1000);
   };
-
+  var delaySchool;
   $scope.changeSchool = function(){
-      $scope.postChanges('school',$scope.userdata.school);    
+    $timeout.cancel(delaySchool);
+    delaySchool = $timeout(function() {
+      $scope.postChanges('school',$scope.userdata.school);
+    }, 1000);    
   };
-
+  var delayPrice;
   $scope.changePrice = function(){
-      $scope.postChanges('price',$scope.userdata.price);   
+    $timeout.cancel(delayPrice);
+    delayPrice = $timeout(function() {
+      $scope.postChanges('price',$scope.userdata.price); 
+    }, 1000);  
   };
-
+  var delayDist;
   $scope.changeDistance = function(){
-      $scope.postChanges('distance',$scope.userdata.distance);   
+    $timeout.cancel(delayDist);
+    delayDist = $timeout(function() {
+      $scope.postChanges('distance',$scope.userdata.distance);
+    }, 1000);   
   };
-
+  var delaySDate;
   $scope.changeStartDate = function(){
-      $scope.postChanges('startDate',$scope.userdata.startDate);   
+    $timeout.cancel(delaySDate);
+    delaySDate = $timeout(function() {
+      $scope.postChanges('startDate',$scope.userdata.startDate);
+    }, 1000);   
   };
-
+  var delayEDate;
   $scope.changeEndDate = function(){
-      $scope.postChanges('endDate',$scope.userdata.endDate);   
+    $timeout.cancel(delayEDate);
+    delayEDate = $timeout(function() {
+      $scope.postChanges('endDate',$scope.userdata.endDate);
+    }, 1000);   
   };
-
+  var delayBike;
   $scope.changeBikestands = function(){
-      $scope.postChanges('bikestands',$scope.userdata.bikestands);   
+    $timeout.cancel(delayBike);
+    delayBike = $timeout(function() {
+      $scope.postChanges('bikestands',$scope.userdata.bikestands);
+    }, 1000);   
   };
-
+  var delayKitchen;
   $scope.changeSeperatekitchen = function(){
-      $scope.postChanges('seperatekitchen',$scope.userdata.seperatekitchen);   
+    $timeout.cancel(delayKitchen);
+    delayKitchen = $timeout(function() {
+      $scope.postChanges('seperatekitchen',$scope.userdata.seperatekitchen);
+    }, 1000);   
   };
-
+  var delayBath;
   $scope.changeSeperatebathroom = function(){
-      $scope.postChanges('seperatebathroom',$scope.userdata.seperatebathroom);   
+    $timeout.cancel(delayBath);
+    delayBath = $timeout(function() {
+      $scope.postChanges('seperatebathroom',$scope.userdata.seperatebathroom);
+    }, 1000);   
   };
-
+  var delayFurn;
   $scope.changeFurniture = function(){
-      $scope.postChanges('furniture',$scope.userdata.furniture);   
+    $timeout.cancel(delayFurn);
+    delayFurn = $timeout(function() {
+      $scope.postChanges('furniture',$scope.userdata.furniture);
+    }, 1000);   
   };
 
-  //needed to cancel the timeout function
-  var delay;
   $scope.postChanges = function(field,value)
   {
-    //cancel current timeout
-    $timeout.cancel(delay);
-    delay = $timeout(function() {
-      console.log(field+'='+value);
+      console.log(value);
       userdata = JSON.parse(window.localStorage['userdata']);
         $http({method: "POST",dataType:"jsonp",url:'http://maicopaulussen.2fh.co/eindwerk/db/changeFilters.php',data : {field: field,value: value , userid: userdata['id']},headers:{'Access-Control-Allow-Origin': '*'}})
         .success(function(data, status, headers, config) {
           console.log(data);
         });
-    }, 1000);
-    
   }
 
   userdata = JSON.parse(window.localStorage['userdata']);
   $http({method: "POST",dataType:"jsonp",url:'http://maicopaulussen.2fh.co/eindwerk/db/getUser.php',data : {userid: userdata['id']},headers:{'Access-Control-Allow-Origin': '*'}})
         .success(function(data, status, headers, config) {
-          console.log(data['result'][0]);
+          console.log(data['result'][0]['bikestands']);
           if(data['succes'])
           {
             $scope.userdata = data['result'][0];
             $scope.userdata.startDate = new Date(data['result'][0]['startDate']);
             $scope.userdata.endDate = new Date(data['result'][0]['endDate']);
+            $scope.userdata.bikestands = (data['result'][0]['bikestands'] == 1) ? true : false;
+            $scope.userdata.seperatekitchen = (data['result'][0]['seperatekitchen'] == 1) ? true : false;
+            $scope.userdata.seperatebathroom = (data['result'][0]['seperatebathroom'] == 1) ? true : false;
+            $scope.userdata.furniture = (data['result'][0]['furniture'] == 1) ? true : false;
           }
         });
   $scope.userdata = JSON.parse(window.localStorage['userdata']);
