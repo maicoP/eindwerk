91e8e6ced6559c7cc0b87ccc090646a1639b8fb9
Matthias Verhoeven <matthiasverhoeven@gmail.com>
2015-05-20 14:38:06 +0200

    Password style

--- resources/views/auth/password.blade.php
+++ resources/views/auth/password.blade.php
@@ -1,10 +1,13 @@
-@extends('app')
+@extends('head')
 
 @section('content')
-<div class="container-fluid">
-	<div class="row">
-		<div class="col-md-8 col-md-offset-2">
-			<div class="panel panel-default">
+
+   <div class="header_wrapper">
+        <div class="header">
+            <div class="header_content login_content">
+                <div class="row">
+                    <div class="col-md-8 col-md-offset-2">
+                        <div class="panel panel-default">
 				<div class="panel-heading">Reset Password</div>
 				<div class="panel-body">
 					@if (session('status'))
@@ -43,8 +46,18 @@
 						</div>
 					</form>
 				</div>
-			</div>
-		</div>
-	</div>
-</div>
-@endsection
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <script src="js/jquery.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/jquery.easing.min.js"></script>
+    <script src="js/wow.min.js"></script><!--animeer items vanaf user scrolled-->
+    <script src="js/custom.js"></script>
+
+@stop
