54e5b67d18621cdae9ccf55d232095a89ccdcdbe
Matthias Verhoeven <matthiasverhoeven@gmail.com>
2015-06-10 08:18:24 +0200

    Cleanup index

--- resources/views/kot/index.blade.php
+++ resources/views/kot/index.blade.php
@@ -13,7 +13,6 @@
 				</ul>
 
 				<div class="row-fluid">
-					<div class="span13">
 						
 						<h1>Alle toegevoegde koten</h1>
 						@if(Session::has('message'))
@@ -26,10 +25,10 @@
 			  				<p>Voor meer hulp en informatie navigeert u naar het menu "<b>Help</b>".</p>
 						@endif
 						
-			@if(!$koten->isEmpty())
-				<div class="status approved top_spacing"><span>Koten in het systeem</span></div>
-			@endif
-			@foreach($koten as $kot)
+						@if(!$koten->isEmpty())
+							<div class="status approved top_spacing"><span>Koten in het systeem</span></div>
+						@endif
+						@foreach($koten as $kot)
 						
 						<div class="kot approved">
 							<div class="info">
@@ -50,35 +49,35 @@
 								<div>Aantal likes @if(!$kot->appUserKotCount->isEmpty()){{$kot->appUserKotCount[0]->count}}@else 0 @endif</div>
 							</div>
 						</div>
-			@endforeach					
-				@if(!$new->isEmpty())
-					<div class="status pending"><span>Koten in review</span></div>
-				@endif
-				
-					@foreach($new as $kot)
-						<div class="kot pending">
-							<div class="info">
-								<div class="title"><a href="/kot/{{$kot->id}}">{{$kot->streatname}}  {{$kot->housenumber}} {{$kot->zipcode}} {{$kot->city}}</a></div>
-								<div><a href="/kot/{{$kot->id}}"><img width="300" src="{{URL::to('/').'/'.$kot->images[0]->image}}" alt="Kot image"></a></div>
-								<div>
-									{!!Form::open(['url' => 'kot/'.$kot->id.'/edit','method' => 'GET','class' => 'frm-edit_delete'])!!}
-										{!!Form::submit('Edit',array('class' => 'btn btn-primary btn-edit_delete'))!!}
-									{!!Form::close()!!}
-									<button type="button" class="btn btn-danger btn-edit_delete" data-toggle="popover" data-placement="top" title="Bent u zeker dat u dit kot wild deleten" data-content="<input class='btn btn-danger btn-block' type='submit' value='Bevestigen' onclick='deleteKot({{$kot->id}})'>">Delete</button>					
-								</div>
-								</div>
-							<div class="details">
-								<div class="price">€{{$kot->price}}</div>
-								<div>{{$kot->size}}m²</div>
-								<div class="kot_info"><br>{{$kot->info}}</div>
-							</div>
-						</div>
-					@endforeach		
 
-					@if(!$declined->isEmpty())
-						<div class="status denied"><span>Geweigerde koten</span></div>
-					@endif
-					
+						@endforeach					
+						@if(!$new->isEmpty())
+							<div class="status pending"><span>Koten in review</span></div>
+						@endif
+				
+						@foreach($new as $kot)
+							<div class="kot pending">
+								<div class="info">
+									<div class="title"><a href="/kot/{{$kot->id}}">{{$kot->streatname}}  {{$kot->housenumber}} {{$kot->zipcode}} {{$kot->city}}</a></div>
+									<div><a href="/kot/{{$kot->id}}"><img width="300" src="{{URL::to('/').'/'.$kot->images[0]->image}}" alt="Kot image"></a></div>
+									<div>
+										{!!Form::open(['url' => 'kot/'.$kot->id.'/edit','method' => 'GET','class' => 'frm-edit_delete'])!!}
+											{!!Form::submit('Edit',array('class' => 'btn btn-primary btn-edit_delete'))!!}
+										{!!Form::close()!!}
+										<button type="button" class="btn btn-danger btn-edit_delete" data-toggle="popover" data-placement="top" title="Bent u zeker dat u dit kot wild deleten" data-content="<input class='btn btn-danger btn-block' type='submit' value='Bevestigen' onclick='deleteKot({{$kot->id}})'>">Delete</button>					
+									</div>
+									</div>
+								<div class="details">
+									<div class="price">€{{$kot->price}}</div>
+									<div>{{$kot->size}}m²</div>
+									<div class="kot_info"><br>{{$kot->info}}</div>
+								</div>
+							</div>
+						@endforeach		
+
+						@if(!$declined->isEmpty())
+							<div class="status denied"><span>Geweigerde koten</span></div>
+						@endif
 					
 						@foreach($declined as $kot)
 							<div class="kot denied">
@@ -99,8 +98,7 @@
 								</div>
 							</div>
 						@endforeach	
-										
-					</div><!--end koten-->
+
 				</div>
 			</div><!--end content-->
 		</div><!--end row fluid-->
