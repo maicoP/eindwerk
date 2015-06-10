2fbc955a2fbbdd6d7b61fa5d1e2efa0c24ece9b8
Matthias Verhoeven <matthiasverhoeven@gmail.com>
2015-06-10 08:23:55 +0200

    Button op edit pagina zoals alle andere paginas + styling btn-info

--- resources/views/kot/edit.blade.php
+++ resources/views/kot/edit.blade.php
@@ -154,7 +154,7 @@
 						</div>
 						<div class="control-group">
 							<div class="controls col-md-offset-4">
-								<button type="submit" class="btn btn-primary" style="margin-right: 15px;">
+								<button type="submit" class="btn btn-info" style="margin-right: 15px;">
 									Wijzigingen opslaan
 								</button>
 							</div>
