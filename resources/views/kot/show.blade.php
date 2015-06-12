0a5201b9f63cbb02b0cd2cb092c043e5573935db
Matthias Verhoeven <matthiasverhoeven@gmail.com>
2015-06-12 16:57:22 +0200

    Show koten

--- resources/views/kot/show.blade.php
+++ resources/views/kot/show.blade.php
@@ -13,61 +13,56 @@
 				</ul>
 			
 				<div class="row-fluid sortable">
-					
+
 					<h1>Kot detail: {{$kot->streatname}}  {{$kot->housenumber}} {{$kot->zipcode}} {{$kot->city}}</h1>
-					
+
 					<div class="top_spacing">
-					<div>
-						{!!Form::open(['url' => 'kot/'.$kot->id.'/edit','method' => 'GET','class' => 'frm-edit_delete'])!!}
-							{!!Form::submit('Edit',array('class' => 'btn btn-primary btn-edit_delete'))!!}
-						{!!Form::close()!!}
-						<button type="button" class="btn btn-danger btn-edit_delete" data-toggle="popover" data-placement="top" title="Bent u zeker dat u dit kot wild deleten" data-content="<input class='btn btn-danger btn-block' type='submit' value='Bevestigen' onclick='deleteKot({{$kot->id}})'>">Delete</button>							
-					</div>
+
+								<div>
+									{!!Form::open(['url' => 'kot/'.$kot->id.'/edit','method' => 'GET','class' => 'frm-edit_delete'])!!}
+										{!!Form::submit('Edit',array('class' => 'btn btn-primary btn-edit_delete'))!!}
+									{!!Form::close()!!}
+									<button type="button" class="btn btn-danger btn-edit_delete" data-toggle="popover" data-placement="top" title="Bent u zeker dat u dit kot wild deleten" data-content="<input class='btn btn-danger btn-block' type='submit' value='Bevestigen' onclick='deleteKot({{$kot->id}})'>">Delete</button>							
+								</div>
 
 
-<div class="container-fluid">
-	<div class="row">
-		@foreach($kot->images as $image)
-			<img src="{{URL::to('/').'/'.$image->image}}" width="350" alt="">
-		@endforeach
-		<div>
+								@foreach($kot->images as $image)
+									<img src="{{URL::to('/').'/'.$image->image}}" width="350" alt="">
+								@endforeach
+								<div>
 
-		</div>
-		<div>
-			<h2>Info</h2>
-			<p>Prijs (€): <b>{{$kot->price}}</b></p>
-			<p>Oppervlakte (m²): <b>{{$kot->size}}</b></p>
-			<p>Adres: <b>{{$kot->streatname}}  {{$kot->housenumber}} {{$kot->zipcode}} {{$kot->city}}</b></p>
-			<p>Beschikbaar van <b>{{$kot->begindate}}</b> tot <b>{{$kot->enddate}}</b></p>
-			<p>Beschrijving / Info: <b>{{$kot->info}}</b></p>
-			<h2>Opties</h2>
-			<ul> 				
-				@if($kot->bikestands)
-					<li>Fietsenstalling</li> 
-				@endif
-				@if($kot->seperatekitchen)
-					<li>Aparte keuken</li>
-				@endif
-				@if($kot->seperatebathroom) 
-					<li>Aparte Badkamer</li>
-				@endif
-				@if($kot->furniture)
-					<li>Bemeubeld</li>
-				@endif
-				@if($kot->wifi)
-					<li>wifi</li>
-				@endif
-		</ul>
-			<h2>Contact</h2>
-			<p>Naam: <b>{{$kot->name}}</b></p>
-			<p>E-mail: <b>{{$kot->email}}</b></p>
-			<p>Telefoonnummer: <b>{{$kot->telephonenumber}}</b></p>
+								</div>
+								<div>
+									<h2>Info</h2>
+									<p>Prijs (€): <b>{{$kot->price}}</b></p>
+									<p>Oppervlakte (m²): <b>{{$kot->size}}</b></p>
+									<p>Adres: <b>{{$kot->streatname}}  {{$kot->housenumber}} {{$kot->zipcode}} {{$kot->city}}</b></p>
+									<p>Beschikbaar van <b>{{$kot->begindate}}</b> tot <b>{{$kot->enddate}}</b></p>
+									<p>Beschrijving / Info: <b>{{$kot->info}}</b></p>
+									<h2>Opties</h2>
+									<ul> 				
+										@if($kot->bikestands)
+											<li>Fietsenstalling</li> 
+										@endif
+										@if($kot->seperatekitchen)
+											<li>Aparte keuken</li>
+										@endif
+										@if($kot->seperatebathroom) 
+											<li>Aparte Badkamer</li>
+										@endif
+										@if($kot->furniture)
+											<li>Bemeubeld</li>
+										@endif
+										@if($kot->wifi)
+											<li>wifi</li>
+										@endif
+								</ul>
+									<h2>Contact</h2>
+									<p>Naam: <b>{{$kot->name}}</b></p>
+									<p>E-mail: <b>{{$kot->email}}</b></p>
+									<p>Telefoonnummer: <b>{{$kot->telephonenumber}}</b></p>
 
-		</div>
-	</div>
-</div>
-
-
+								</div>
 					</div><!--end top spacing-->
 				</div><!--end row fluid-->
 			</div><!--end content-->
