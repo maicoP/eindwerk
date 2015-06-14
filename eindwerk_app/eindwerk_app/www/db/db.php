0253b8ce21feff63a298b1694105e44363990b70
Matthias Verhoeven <matthiasverhoeven@gmail.com>
2015-06-14 17:34:00 +0200

    App css cleanen, delete duplicate folder

--- eindwerk_app/eindwerk_app/www/db/db.php
+++
@@ -1,9 +0,0 @@
-<?php 
-$host="fdb13.biz.nf"; // Host name
-$username="1847346_eindwerk"; // Mysql username
-$password="mm22mm33"; // Mysql password
-$db_name="1847346_eindwerk"; // Database name
-
-// Connect to server and select database.
-$con = mysqli_connect($host, $username, $password)or die("cannot connect");
-mysqli_select_db($con,$db_name)or die("cannot select DB");
