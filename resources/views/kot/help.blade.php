31c1100292cd4d7c9fe3e01ac45d63d4e5914826
Matthias Verhoeven <matthiasverhoeven@gmail.com>
2015-06-14 10:44:48 +0200

    Help update

--- resources/views/kot/help.blade.php
+++ resources/views/kot/help.blade.php
@@ -19,41 +19,34 @@
 						
 						<h2><br>Verificatiesysteem</h2>
 
-						Om de kwaliteit van de koten in de applicatie te garanderen gaat het team achter Kotter koten individueel bekijken, indien een kot aan alle voorwaarden voldoet, zullen wij het goedkeuren en zal het kot in de app verschijnen. Een verificatie gebeurd binnen 24u. Terwijl een kot gereviewed wordt zal het opgelijst worden onder 'in review' in het kotenoverzicht.
-						Indien een kot wordt afgekeurd zullen wij een reden geven waarom. Het kot wordt dan opgelijst onder de 'geweigerde koten'.
+Om de kwaliteit van de koten in de applicatie te garanderen gaat het Kotter team koten individueel bekijken vooraleer ze in de app toe te voegen. 
+Indien een kot aan alle voorwaarden voldoet, zullen wij het goedkeuren en zal het kot in de app verschijnen. Een verificatie gebeurd binnen 24u. Terwijl een kot gereviewed wordt zal het opgelijst worden onder 'in review' in het kotenoverzicht. Indien een kot wordt afgekeurd zullen wij een reden geven waarom. Het kot wordt dan opgelijst onder de 'geweigerde koten'.
 
 						<h1><br>Handleiding</h1>
 
 						<h2><br>Koten toevoegen</h2>
 
-						Een kot toevoegen doet u via het menu in de zijbalk genaamd 'kot toevoegen'. Het proces wijst zichzelf uit, het enige wat we nodig hebben zijn alle gegevens en 4 kwaliteitsvolle foto's die een beeld geven van het kot. (Een foto met een breedhoeklens/fisheye heeft de voorkeur aangezien deze meer kunnen laten zien binnenin een kamer.)
+Een kot toevoegen doet u via het menu in de zijbalk genaamd 'kot toevoegen'. Het proces wijst zichzelf uit, het enige wat we nodig hebben zijn alle gegevens en 4 kwaliteitsvolle foto's die een beeld geven van het kot. (Een foto met een breedhoeklens/fisheye heeft de voorkeur aangezien deze meer kunnen laten zien binnenin een kamer.)
+
+<b>Prijs</b>
+Elk kot moet verplicht de totaalprijs laten zien. Indien een vaste totale prijs niet mogelijk is bij een kot, moet er een geschatte totale prijs gegeven worden (vink hiervoor ook het veld 'schatting' aan). Deze geschatte prijs moet zo realistisch mogelijk zijn. Misbruik hiervan zal worden bestraft.
 
 						<h2><br>Koten bewerken</h2>
 
-						Als u een aanpassing wil doen aan een bestaand kot (inclusief foto's aanpassen, contactgegevens aanpassen,..), dan kan u dat zeer eenvoudig door naar het kotenoverzicht te gaan en onder het kot ik kwestie op de 'edit' knop te klikken.
+Als u een aanpassing wil doen aan een bestaand kot (inclusief foto's aanpassen, contactgegevens aanpassen,..), dan kan u dat zeer eenvoudig door naar het kotenoverzicht te gaan en onder het kot in kwestie op de 'edit' knop te klikken.
 
 						<h2><br>Koten deleten</h2>
 
-						-zie koten op inactief zetten indien u een kot tijdelijk uit het systeem wilt halen-
-
-						Indien u een kot permanent wenst te verwijderen uit de app, dan kan u dit doen door naar het kotenoverzicht te gaan en onder het bewuste kot op de 'delete' knop te klikken.
-
-						<h2><br>Koten uit het systeem halen (niet permanent verwijderen)</h2>
-
-						Als u wenst een kot tijdelijk uit het systeem te halen, dan kan u dat doen door naar het kotenoverzicht te gaan, op 'edit' klikken onder het kot, en status op 'inactief' te zetten.
+Indien u een kot permanent wenst te verwijderen uit de app, dan kan u dit doen door naar het kotenoverzicht te gaan en onder het bewuste kot op de 'delete' knop te klikken.
 
 						<h1><br>Richtlijnen koten</h1>
 
-						Kotter staat voor de net-iets-meer koten, de kuxueuzere koten. Daarom zijn hier enkele richtlijnen zodat u zelf kan controleren of een kot aan de richtlijnen voldoet alvorens het in het systeem in te geven.<br><br>
-
 						<ul>
-						<li>Het kot heeft een minimum oppervlakte van x m2.</li>
-						<li>Het kot heeft een eigen badkamer (of in geval van een duplex: maximum 2 personen voor 1 badkamer)</li>
+						<li>Het kot heeft een minimum oppervlakte van 10 m2.</li>
+						<li>De kotprijs is een totaalprijs met alle kosten inbegrepen. Dit mag ook een schatting zijn.</li>
 						<li>Het kot bevind zich in zeer goede staat</li>
-						<li>Het kot heeft een eigen keuken (of in geval van een duplex: maximum 2 personen voor 1 keuken).</li>
+						<li>Het kot beschikt over 4 duidelijke foto's</li>
 						</ul>
-
-						Indien uw kot aan al deze voorwaarden voldoet is de kans zeer klein dat het door ons wordt afgekeurd.
 
 						<h1><br>Contact</h1>
 
